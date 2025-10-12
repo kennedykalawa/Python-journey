388c132cafda74ee081fda49bd4f7d6db0949c84
Kennedy <156435591+kennedykalawa@users.noreply.github.com>
2025-10-12 12:19:25 +0300

    calc

---
+++ Beginner/basic calc.py
@@ -0,0 +1,8 @@
+num1 = input("Enter your first number: ")
+num2 = input("Enter your second numnber: ")
+result = num1 + num2
+""" result2 = int(num1 + num2)
+ """
+result2 = int(num1) + int(num2)
+print(result)
+print(result2)
