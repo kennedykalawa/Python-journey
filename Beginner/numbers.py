a82d5c36b3291c4cc145c0a981d32be711fb95a6
Kennedy <156435591+kennedykalawa@users.noreply.github.com>
2025-10-12 11:56:18 +0300

    12/10/25 11:56am

--- Beginner/numbers.py
+++ Beginner/numbers.py
@@ -3,5 +3,11 @@ print(3 * 4 + 6)
 print(3 * (4+6))
 print(10%4) """
 
-my_num = [1,2,3]
-print(str(my_num) + " ""is my favourite")
+""" my_num = [1,2,3]
+print(str(my_num) + " ""is my favourite") """
+
+my_num = 2
+print(abs(my_num))
+print(pow(my_num,2))
+print(min(6,8))
+print(max(6,8))
