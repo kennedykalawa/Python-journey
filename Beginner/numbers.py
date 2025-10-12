d9215621f94348e8efad378b08e202129a81bbd9
Kennedy <156435591+kennedykalawa@users.noreply.github.com>
2025-10-12 11:59:29 +0300

    import math

--- Beginner/numbers.py
+++ Beginner/numbers.py
@@ -1,3 +1,4 @@
+from math import *
 """ print(546 + 56)
 print(3 * 4 + 6)
 print(3 * (4+6))
@@ -6,8 +7,10 @@ print(10%4) """
 """ my_num = [1,2,3]
 print(str(my_num) + " ""is my favourite") """
 
-my_num = 2
+""" my_num = 2
 print(abs(my_num))
 print(pow(my_num,2))
 print(min(6,8))
-print(max(6,8))
+print(max(6,8)) """
+
+print(sqrt(49))
