d97fd14923737eed08fe1dc538f2e061ea689a8c
Kennedy <156435591+kennedykalawa@users.noreply.github.com>
2025-10-12 11:35:36 +0300

    morning session work

---
+++ Beginner/app.py
@@ -0,0 +1,15 @@
+""" print("This is the best techie ghost-engineer revisiting python beginner course")
+print("I am learning python from the beginning")
+
+print("  / |")
+print(" /  |")
+print("/___|") """ 
+
+character_name = "James"
+character_age = "35"
+print("Ther once was a man named" " "+ character_name + ",")
+print("he was" " "+ character_age + "years old.")
+character_name = "Mike"
+character_age = "70"
+print("He really liked the name" " "+ character_name + ",")
+print("but didn't like being" " "+ character_age + ".")
---
+++ Beginner/string.py
@@ -0,0 +1,10 @@
+phrase = "My Academy"
+"""print(phrase +  "is getting there")
+print(phrase.upper().isupper())
+print(phrase.lower()) 
+print(len(phrase))"""
+print(phrase[0])
+print(phrase[1])
+print(phrase.index("A"))
+
+print(phrase.replace("My","klw"))
