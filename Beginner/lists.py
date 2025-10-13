7f628ececd830bce71bf79c998fc1a1331c877f5
Kennedy <156435591+kennedykalawa@users.noreply.github.com>
2025-10-13 18:25:12 +0300

    list functions

--- Beginner/lists.py
+++ Beginner/lists.py
@@ -1,8 +1,31 @@
 Friends = ["rosch","kev","jim", "Joey","Rachel"]
-print(Friends)
+numbers = [1,2,3,4,5]
+""" print(Friends)
 
 print(Friends[0])
 print(Friends[1])
 print(Friends[2])
 
-print(Friends[1:3])
+print(Friends[1:3]) """
+
+""" Friends.extend(numbers)
+print(Friends)
+Friends.append("phebe")
+print(Friends)
+numbers.insert(0, 0)
+print(numbers)
+Friends.remove("rosch")
+print(Friends)
+Friends.pop()
+print(Friends) """
+
+Friends.sort()
+""" numbers.sort()
+print(Friends)
+print(numbers) """
+
+numbers2 = numbers.copy()
+print(numbers2)
+numbers2.reverse()
+print(numbers2)
+
