2d50ebdc59b7c6796fd8931c983f546196f44844
kennedykalawa <kalawamkennedy@gmail.com>
2026-03-04 17:26:55 +0300

    string

---
+++ Beginner2/strings.py
@@ -0,0 +1,11 @@
+course = "Python for beginners by Mosh Hemadini"
+print(course)
+name = input("What is your name? ")
+
+course1 = ("Hello ")  
+course2 =(""" this is the beginner's course for python
+          welcome to mosh's school
+          here you will learn all the hands-on skills you require.
+""")
+
+print((course1 + name)+ (course2))
