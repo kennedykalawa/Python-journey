6f17f9bef0ab5dd19695c2465c38c985baa98c85
kennedykalawa <kalawamkennedy@gmail.com>
2026-03-05 15:06:07 +0300

    add string manipulation examples for beginners

---
+++ Beginner2/string method.py
@@ -0,0 +1,8 @@
+course = "Python for beginners"
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find("o"))
+print(course.replace("beginners", "absolute beginners"))
+
+print("Python" in course)
